1d405d4f12dd0d06540ed52daee8a28920d2e23a
karthikn-pk <pkarthikeyanp33@gmail.com>
2024-05-23 23:56:03 +0530

    configuring routes

--- BigStack/index.js
+++ BigStack/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+//bring all routes
+const auth = require("./routes/api/auth");
+const profile = require("./routes/api/profile");
+const question = require("./routes/api/question");
 
 const app = express();
 const port = 3000;
 
+//middleware for bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//mongoDB config
+const db = require("./setup/myurl").mongoURL;
+
+//attempt to connect database
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongodb connected sucessfully"))
+  .catch((error) => console.error(error));
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello </h1>");
 });
+
+//routes
+app.use("/api/auth", auth);
+
+app.use("/api/profile", profile);
+app.use("/api/question", question);
 
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
---
+++ BigStack/setup/myurl.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURL:
+    "mongodb+srv://pkarthikeyanp33:3ddeoeXawznSQsXH@karthidb.jgxmnko.mongodb.net",
+  secret: "thisiskarthi",
+};
